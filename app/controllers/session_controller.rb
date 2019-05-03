bc0cd315df702e1df5e5eef7a0dd1e8ed77589a6
fralps <francois.loupias@orange.fr>
2019-05-03 18:21:35 +0200

    Cookies OK

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,26 +2,20 @@ class SessionController < ApplicationController
 	def new
 	end
 
-	def create
-
-		user = User.find_by(email: params[:email])
-
-		if user && user.authenticate(params[:password])
-			log_in(user)
-			remember_user
-			flash[:success] = "Welcome back, #{user.first_name}! 👍"
-			redirect_to root_path
-		else
-
-			flash[:error] = "Wrong login / password combination"
-			render 'new'
-		end
-	end
+	  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 
 	def destroy
-		forget(current_user)
 		session.delete(:user_id)
-		@current_user = nil
 		redirect_to root_path
 	end
 end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,16 +1,5 @@
 module SessionHelper
 
-	def log_in(user)
-		session[:user_id] = user.id
-	end
-
-		 # Remembers a user in a persistent session.
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
 	 # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
@@ -24,7 +13,30 @@ module SessionHelper
     end
   end
 
-  # Forgets a persistent session.
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	# Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+    # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Logs out the current user.
+  def log_out
+  	forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+    # Forgets a persistent session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
